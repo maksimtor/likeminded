78bb512b5eeff49c222dbed6c9736d1763fc54c9
maksimtor <toropyginmaxim@gmail.com>
2023-07-12 12:39:18 +0600

    chat: refactor consumers

--- chat/consumers.py
+++ chat/consumers.py
@@ -3,6 +3,7 @@ from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 from .models import CustomUser as User
 from .models import Chat
+from chat.tools.searchAlgorithm import waiting_for_invite, search_to_invite
 from django.contrib.auth.models import User as RealUser
 from chat.tools.prefAlgorithm import calcAcceptance, calcLikeness, AcceptanceCalculator, LikenessCalculator
 import time
@@ -10,25 +11,17 @@ import threading
 
 class ChatSearchConsumer(WebsocketConsumer):
     def connect(self):
-        # print("Hi")
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
-
-        # Join room group
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
-
         self.accept()
 
     def disconnect(self, close_code):
-        # Delete room from db
-        # chats = Chat.objects.filter(id=self.room_name).delete()
-        print("Deleting custom user " + self.room_name)
         user = User.objects.get(pk=int(self.room_name))
         if (user.user == None):
-            print("Deleted")
             user.delete()
         else:
             user.status='Stopped'
@@ -44,7 +37,6 @@ class ChatSearchConsumer(WebsocketConsumer):
                 'type': 'exit_message'
             }
         )
-        # Leave room group
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
@@ -55,75 +47,11 @@ class ChatSearchConsumer(WebsocketConsumer):
         user = User.objects.get(pk=int(user_id))
         user.status = 'Searching'
         user.save()
-        print (user.name + "Start waiting for ")
-        for i in range (0,10):
-            print(i)
-            time.sleep(1)
-            if User.objects.get(pk=user.pk).status == 'Invited':
-                print ("User " + user.name + " is invited to " + str(User.objects.get(pk=user.pk).room_to_join))
-                self.send(text_data=json.dumps({
-                    'type': 'chat_message',
-                    'message': User.objects.get(pk=user.pk).room_to_join,
-                    'name': 'name'
-                }))
-                return 1
-            if User.objects.get(pk=user.pk).status == 'Stopped':
-                return 1
+        print (user.name + " starts waiting for invite")
+        waiting_for_invite(self, user, 10)
+        print (user.name + " starts searching users")
+        search_to_invite(self, user)
 
-        while User.objects.get(pk=user.pk).status == 'Searching':
-            print(user.name + "Start searching")
-            searching_users = User.objects.filter(status="Searching")
-            best_user = None
-            best_score = 0
-            for target_user in searching_users:
-                main_accepts_target = AcceptanceCalculator(main_user=user, target_user=target_user)
-                target_accepts_main = AcceptanceCalculator(main_user=target_user, target_user=user)
-                users_new_match = main_accepts_target.users_match() and target_accepts_main.users_match()
-                print("Listen!")
-                print(main_accepts_target.users_match())
-                print(target_accepts_main.users_match())
-                users_old_match = calcAcceptance(mainUser=user, targetUser=target_user) == 1 and calcAcceptance(mainUser=target_user, targetUser=user) == 1
-                if (users_new_match and user.id != target_user.id and target_user not in user.ignored_users.all() and target_user not in user.usersIgnoredBy.all()):
-                    l1 = calcLikeness(mainUser=user, targetUser=target_user)
-                    l2 = calcLikeness(mainUser=target_user, targetUser=user)
-                    result = (l1+l2)/2
-                    if result>best_score:
-                        best_score = result
-                        best_user = target_user.pk
-                if User.objects.get(pk=user.pk).status == 'Invited':
-                    print ("User " + user.name + " is invited to " + str(User.objects.get(pk=user.pk).room_to_join))
-                    self.send(text_data=json.dumps({
-                        'type': 'chat_message',
-                        'message': User.objects.get(pk=user.pk).room_to_join,
-                        'name': 'name'
-                    }))
-                    return 1
-                if User.objects.get(pk=user.pk).status == 'Stopped':
-                    return 1
-            print("Finish searching")
-            if best_score > 0 and User.objects.get(pk=best_user).status == "Searching":
-                print("Getting to the room")
-                chat = Chat()
-                chat.save()
-                User.objects.get(pk=best_user).room_to_join = chat.id
-                User.objects.get(pk=best_user).status = "Invited"
-                for_sure = User.objects.get(pk=best_user)
-                for_sure.room_to_join = chat.id
-                for_sure.status = "Invited"
-                for_sure.save()
-                myself = User.objects.get(pk=user.pk)
-                myself.status="Inactive"
-                myself.save()
-                User.objects.get(pk=user.pk).status = "Inactive"
-                print ("User " + str(user.name) + " created chat " + str(chat.id))
-                self.send(text_data=json.dumps({
-                    'type': 'chat_message',
-                    'message': chat.id,
-                    'name': 'name'
-                }))
-                return 1
-
-    # Receive message from WebSocket
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
@@ -135,12 +63,10 @@ class ChatSearchConsumer(WebsocketConsumer):
 
         self.threadToStop = t
 
-    # Receive message from room group
     def chat_message(self, event):
         message = event['message']
         name = event['name']
 
-        # Send message to WebSocket
         self.send(text_data=json.dumps({
             'type': 'chat_message',
             'message': message,
@@ -148,36 +74,24 @@ class ChatSearchConsumer(WebsocketConsumer):
         }))
 
     def exit_message(self, event):
-        # Send message to WebSocket
         self.send(text_data=json.dumps({
             'type': 'exit_message'
         }))
 
 
-
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
-        # print("Hi")
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
-        # Join room group
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
 
         self.accept()
-        # for i in range(0,5):
-        #     time.sleep(1)
-        # self.send(text_data=json.dumps({
-        #     'type': 'chat_message',
-        #     'message': 'You are connected hmm',
-        #     'name': 'name'
-        # }))
 
     def disconnect(self, close_code):
-        # Delete room from db
         chats = Chat.objects.filter(id=self.room_name).delete()
         self.send(text_data=json.dumps({
             'message': '',
@@ -190,13 +104,11 @@ class ChatConsumer(WebsocketConsumer):
                 'type': 'exit_message'
             }
         )
-        # Leave room group
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
         )
 
-    # Receive message from WebSocket
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
@@ -209,13 +121,12 @@ class ChatConsumer(WebsocketConsumer):
                     'message': message,
                     'name': name
                 }
-            )        # Send message to room group
+            )
 
     def chat_message(self, event):
         message = event['message']
         name = event['name']
 
-        # Send message to WebSocket
         self.send(text_data=json.dumps({
             'type': 'chat_message',
             'message': message,
@@ -223,7 +134,6 @@ class ChatConsumer(WebsocketConsumer):
         }))
 
     def exit_message(self, event):
-        # Send message to WebSocket
         self.send(text_data=json.dumps({
             'type': 'exit_message'
         }))
---
+++ chat/tools/searchAlgorithm.py
@@ -0,0 +1,75 @@
+import time
+import json
+from chat.models import CustomUser as User
+from chat.models import Chat
+from chat.tools.prefAlgorithm import calcAcceptance, calcLikeness, AcceptanceCalculator, LikenessCalculator
+
+def waiting_for_invite(consumer, user, wait_time):
+	for i in range (0, wait_time):
+		print(i)
+		time.sleep(1)
+		if User.objects.get(pk=user.pk).status == 'Invited':
+			print ("User " + user.name + " is invited to " + str(User.objects.get(pk=user.pk).room_to_join))
+			consumer.send(text_data=json.dumps({
+					'type': 'chat_message',
+					'message': User.objects.get(pk=user.pk).room_to_join,
+					'name': 'name'
+				}))
+			return 1
+		if User.objects.get(pk=user.pk).status == 'Stopped':
+			return 1
+
+def search_to_invite(consumer, user):
+	while User.objects.get(pk=user.pk).status == 'Searching':
+		searching_users = User.objects.filter(status="Searching")
+		(best_user, best_score) = find_most_like_minded(user, searching_users, consumer)
+		if best_score > 0 and User.objects.get(pk=best_user).status == "Searching":
+			join_the_room(user, best_user, consumer)
+			return 1
+
+def find_most_like_minded(user, searching_users, consumer):
+	best_user = None
+	best_score = 0
+	for target_user in searching_users:
+		main_accepts_target = AcceptanceCalculator(main_user=user, target_user=target_user)
+		target_accepts_main = AcceptanceCalculator(main_user=target_user, target_user=user)
+		users_match = main_accepts_target.users_match() and target_accepts_main.users_match()
+		if (users_match and user.id != target_user.id and target_user not in user.ignored_users.all() and target_user not in user.usersIgnoredBy.all()):
+			l1m = LikenessCalculator(main_user=user, target_user=target_user)
+			l2m = LikenessCalculator(main_user=target_user, target_user=user)
+			l1 = l1m.calc_likeness()
+			l2 = l2m.calc_likeness()
+			result = (l1+l2)/2
+			if result>best_score:
+				best_score = result
+				best_user = target_user.pk
+		if User.objects.get(pk=user.pk).status == 'Invited':
+			print ("User " + user.name + " is invited to " + str(User.objects.get(pk=user.pk).room_to_join))
+			consumer.send(text_data=json.dumps({
+				'type': 'chat_message',
+				'message': User.objects.get(pk=user.pk).room_to_join,
+				'name': 'name'
+			}))
+			return 1
+		if User.objects.get(pk=user.pk).status == 'Stopped':
+			return 1
+	return (best_user, best_score)
+
+def join_the_room(user, best_user, consumer):
+	print("Getting to the room")
+	chat = Chat()
+	chat.save()
+	matching_user = User.objects.get(pk=best_user)
+	matching_user.room_to_join = chat.id
+	matching_user.status = "Invited"
+	matching_user.save()
+	myself = User.objects.get(pk=user.pk)
+	myself.status="Inactive"
+	myself.save()
+	# User.objects.get(pk=user.pk).status = "Inactive"
+	print ("User " + str(user.name) + " created chat " + str(chat.id))
+	consumer.send(text_data=json.dumps({
+		'type': 'chat_message',
+		'message': chat.id,
+		'name': 'name'
+	}))
