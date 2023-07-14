658329965af6e727f1ccdf13c7b306a4a7127715
maksimtor <toropyginmaxim@gmail.com>
2023-07-14 12:22:52 +0600

    chat: refactoring views
    move some code from views to separate file

---
+++ chat/tools/userModTools.py
@@ -0,0 +1,199 @@
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+from chat.models import CustomUser, Gender, ChatGoal, AgePref, Personality, PolitCoordinates, GeoCoordinates, UserInfo, Preferences
+
+def convert_user_to_json(user):
+	custom_user = user.profile
+	print(custom_user)
+	photo = 'None'
+	if (custom_user.user_info.photo):
+		photo = custom_user.user_info.photo.url
+	user_data = {
+		'name': user.username,
+		'age': custom_user.user_info.age,
+		'gender': custom_user.user_info.gender,
+		'interests': custom_user.user_info.interests,
+		'locToggle': custom_user.user_info.location != None,
+		'geoLat': custom_user.user_info.location.lat,
+		'geoLon': custom_user.user_info.location.lon,
+		'polEco': custom_user.user_info.polit_coordinates.eco*10,
+		'polGov': custom_user.user_info.polit_coordinates.cult*10,
+		'personalityExtraversion': custom_user.user_info.personality.extraversion*10,
+		'personalityAgreeableness': custom_user.user_info.personality.agreeableness*10,
+		'personalityOpenness': custom_user.user_info.personality.openness*10,
+		'personalityConscientiousness': custom_user.user_info.personality.conscientiousness*10,
+		'personalityNeuroticism': custom_user.user_info.personality.neuroticism*10,
+		'politPref': custom_user.user_prefs.polit,
+		'intPref': custom_user.user_prefs.interests,
+		'locPref': custom_user.user_prefs.location,
+		'areaPref': custom_user.user_prefs.loc_area,
+		'areaRestrictToggle': custom_user.user_prefs.area_restrict,
+		'persPref': custom_user.user_prefs.personality,
+		'goals': custom_user.user_prefs.goals,
+		'genderPref': custom_user.user_prefs.gender,
+		'ageRange': [custom_user.user_prefs.age.min_age, custom_user.user_prefs.age.max_age],
+		'ageOptimal': custom_user.user_prefs.age.optimal_age,
+		'description': custom_user.user_info.description,
+		'photo': photo,
+	}
+	print(user_data)
+	return JsonResponse(user_data)
+
+def create_empty_user(data):
+	username = data['username']
+	email = data['email']
+	password = data['password']
+	hu = User.objects.create_user(username=username, email=email, password=password)
+	hu.save()
+	pc = PolitCoordinates.objects.create(eco=0, cult=0)
+	pc.save()
+	gc = GeoCoordinates.objects.create(lat=0, lon=0)
+	gc.save()
+	pers = Personality.objects.create(extraversion=0.5, agreeableness=0.5, conscientiousness=0.5, openness=0.5, neuroticism=0.5)
+	pers.save()
+	info = UserInfo.objects.create(polit_coordinates=pc, location=gc, personality=pers)
+	info.save()
+	ap = AgePref.objects.create(min_age=18, max_age=100, optimal_age=25)
+	ap.save()
+	prefs = Preferences.objects.create(age=ap)
+	prefs.save()
+	u = CustomUser.objects.create(user=hu, user_info=info, user_prefs=prefs)
+	u.save()
+
+def create_user_with_profile(data):
+	print (data)
+	userGender = data['gender']['value']
+	if (userGender == 'M'):
+		userGender = Gender.MALE
+	elif (userGender == 'F'):
+		userGender = Gender.FEMALE
+	else:
+		userGender = Gender.ANYTHING
+	userGoals = data['goals']['value']
+	if (userGoals == 'FR'):
+		userGoals = ChatGoal.FRIENDSHIP
+	elif (userGoals == 'RO'):
+		userGoals = ChatGoal.ROMANTIC
+	else:
+		userGoals = ChatGoal.ANYTHING
+	userGenderPref = data['genderPref']['value']
+	if (userGenderPref == 'M'):
+		userGenderPref = Gender.MALE
+	elif (userGenderPref == 'F'):
+		userGenderPref = Gender.FEMALE
+	else:
+		userGenderPref = Gender.ANYTHING
+	age_pref = AgePref.objects.create(min_age=data['ageRange'][0], max_age=data['ageRange'][1], optimal_age=data['ageOptimal'])
+	age_pref.save()
+	personality = Personality.objects.create(
+		extraversion=data['personalityExtraversion']/10,
+		agreeableness=data['personalityAgreeableness']/10,
+		openness=data['personalityOpenness']/10,
+		conscientiousness=data['personalityConscientiousness']/10,
+		neuroticism=data['personalityNeuroticism']/10
+	)
+	personality.save()
+	pol = PolitCoordinates.objects.create(eco=data['polEco']/10, cult=data['polGov']/10)
+	pol.save()
+	geo = None
+	if (data['locToggle']):
+		print('save geo')
+		geo = GeoCoordinates.objects.create(lat=data['geoLat'], lon=data['geoLon'])
+		geo.save()
+	user_info = UserInfo.objects.create(
+		interests=[i['value'] for i in data['interests']] if 'interests' in data.keys() else [],
+		polit_coordinates=pol,
+		location=geo,
+		age=data['age'] if isinstance(data['age'], int) else None,
+		gender=userGender,
+		personality=personality
+		)
+	user_info.save()
+	user_pref = Preferences.objects.create(
+		goals=userGoals,
+		age=age_pref,
+		polit=data['politPref'],
+		interests=data['intPref'],
+		location=data['locPref'],
+		gender=userGenderPref,
+		personality=data['persPref'],
+		area_restrict=data['areaRestrictToggle'],
+		loc_area=data['areaPref'],
+		)
+	user_pref.save()
+	custom_user = CustomUser.objects.create(name=data['name'], user_info=user_info, user_prefs=user_pref)
+	custom_user.save()
+	return custom_user
+
+def update_user(data):
+	userGender = data['gender']['value']
+	if (userGender == 'M'):
+		userGender = Gender.MALE
+	elif (userGender == 'F'):
+		userGender = Gender.FEMALE
+	else:
+		userGender = Gender.ANYTHING
+	userGoals = data['goals']['value']
+	if (userGoals == 'FR'):
+		userGoals = ChatGoal.FRIENDSHIP
+	elif (userGoals == 'RO'):
+		userGoals = ChatGoal.ROMANTIC
+	else:
+		userGoals = ChatGoal.ANYTHING
+	userGenderPref = data['genderPref']['value']
+	if (userGenderPref == 'M'):
+		userGenderPref = Gender.MALE
+	elif (userGenderPref == 'F'):
+		userGenderPref = Gender.FEMALE
+	else:
+		userGenderPref = Gender.ANYTHING
+	user = User.objects.get(id=data['user_id'])
+	user.username = data['name']
+	profile = user.profile
+	info = profile.user_info
+	info.interests=[i['value'] for i in data['interests']] if 'interests' in data.keys() else []
+	info.description=data['description']
+	pol = info.polit_coordinates
+	pol.eco=data['polEco']/10
+	pol.cult=data['polGov']/10
+	pol.save()
+	info.polit_coordinates=pol
+	geo = info.location
+	geo.lat=data['geoLat']
+	geo.lon=data['geoLon']
+	geo.save()
+	info.location=geo
+	info.age=data['age']
+	print(info.location.lat)
+	info.gender=userGender
+	personality=info.personality
+	personality.extraversion=data['personalityExtraversion']/10
+	personality.agreeableness=data['personalityAgreeableness']/10
+	personality.openness=data['personalityOpenness']/10
+	personality.conscientiousness=data['personalityConscientiousness']/10
+	personality.neuroticism=data['personalityNeuroticism']/10
+	print(personality.extraversion)
+	personality.save()
+	info.personality=personality
+	info.save()
+
+	prefs = profile.user_prefs
+	prefs.goals=userGoals
+	prefs.polit=data['politPref']
+	prefs.interests=data['intPref']
+	prefs.location=data['locPref']
+	prefs.gender=userGenderPref
+	prefs.personality=data['persPref']
+	prefs.area_restrict=data['areaRestrictToggle']
+	prefs.loc_area=data['areaPref']
+	age_pref = prefs.age
+	age_pref.min_age=data['ageRange'][0]
+	age_pref.max_age=data['ageRange'][1]
+	age_pref.optimal_age=data['ageOptimal']
+	age_pref.save()
+	prefs.age_pref = age_pref
+	prefs.save()
+	profile.user_info = info
+	profile.user_prefs = prefs
+	profile.save()
+	user.save()
--- chat/views.py
+++ chat/views.py
@@ -7,6 +7,7 @@ from django.urls import reverse
 from django.db.models import Q
 from .models import CustomUser, Gender, ChatGoal, AgePref, Personality, PolitCoordinates, GeoCoordinates, UserInfo, Preferences
 from chat.tools.prefAlgorithm import calcAcceptance, calcLikeness
+from chat.tools.userModTools import convert_user_to_json, create_empty_user, create_user_with_profile, update_user
 from .serializers import UserSerializer
 from rest_framework import viewsets
 from django.views.decorators.csrf import csrf_exempt
@@ -36,281 +37,23 @@ class UserView(DetailView):
         print (self)
         print(user_id)
         user = User.objects.get(id=int(user_id))
-        custom_user = user.profile
-        print(custom_user)
-        photo = 'None'
-        if (custom_user.user_info.photo):
-            photo = custom_user.user_info.photo.url
-        user_data = {
-            'name': user.username,
-            'age': custom_user.user_info.age,
-            'gender': custom_user.user_info.gender,
-            'interests': custom_user.user_info.interests,
-            'locToggle': custom_user.user_info.location != None,
-            'geoLat': custom_user.user_info.location.lat,
-            'geoLon': custom_user.user_info.location.lon,
-            'polEco': custom_user.user_info.polit_coordinates.eco*10,
-            'polGov': custom_user.user_info.polit_coordinates.cult*10,
-            'personalityExtraversion': custom_user.user_info.personality.extraversion*10,
-            'personalityAgreeableness': custom_user.user_info.personality.agreeableness*10,
-            'personalityOpenness': custom_user.user_info.personality.openness*10,
-            'personalityConscientiousness': custom_user.user_info.personality.conscientiousness*10,
-            'personalityNeuroticism': custom_user.user_info.personality.neuroticism*10,
-            'politPref': custom_user.user_prefs.polit,
-            'intPref': custom_user.user_prefs.interests,
-            'locPref': custom_user.user_prefs.location,
-            'areaPref': custom_user.user_prefs.loc_area,
-            'areaRestrictToggle': custom_user.user_prefs.area_restrict,
-            'persPref': custom_user.user_prefs.personality,
-            'goals': custom_user.user_prefs.goals,
-            'genderPref': custom_user.user_prefs.gender,
-            'ageRange': [custom_user.user_prefs.age.min_age, custom_user.user_prefs.age.max_age],
-            'ageOptimal': custom_user.user_prefs.age.optimal_age,
-            'description': custom_user.user_info.description,
-            'photo': photo,
-        }
-        print(user_data)
-        return JsonResponse(user_data)
+        return convert_user_to_json(user)
 
     def post(self, request):
         body_unicode = request.body.decode('utf-8')
         data = json.loads(body_unicode)
         if data['registration']:
-            username = data['username']
-            email = data['email']
-            password = data['password']
-            hu = User.objects.create_user(username=username, email=email, password=password)
-            hu.save()
-            pc = PolitCoordinates.objects.create(eco=0, cult=0)
-            pc.save()
-            gc = GeoCoordinates.objects.create(lat=0, lon=0)
-            gc.save()
-            pers = Personality.objects.create(extraversion=0.5, agreeableness=0.5, conscientiousness=0.5, openness=0.5, neuroticism=0.5)
-            pers.save()
-            info = UserInfo.objects.create(polit_coordinates=pc, location=gc, personality=pers)
-            info.save()
-
-            ap = AgePref.objects.create(min_age=18, max_age=100, optimal_age=25)
-            ap.save()
-            prefs = Preferences.objects.create(age=ap)
-            prefs.save()
-            u = CustomUser.objects.create(user=hu, user_info=info, user_prefs=prefs)
-            u.save()
+            create_empty_user(data)
             return JsonResponse({'problems': 'none'})
         else:
-            print (data)
-            userName = data['name']
-            userAge = data['age']
-            userGender = data['gender']['value']
-            userAreaRestrictToggle=data['areaRestrictToggle']
-            userLocPref=data['locPref']
-            userAreaPref=data['areaPref']
-            if (userGender == 'M'):
-                userGender = Gender.MALE
-            elif (userGender == 'F'):
-                userGender = Gender.FEMALE
-            else:
-                userGender = Gender.ANYTHING
-            userInterests = data['interests']
-            userInterestsReformed = []
-            for i in userInterests:
-                userInterestsReformed.append(i['value'])
-            userGeoLat = data['geoLat']
-            userGeoLon = data['geoLon']
-            userLocToggle = data['locToggle']
-            userPolEco = data['polEco']/10
-            userPolGov = data['polGov']/10
-            userPersonalityExtraversion = data['personalityExtraversion']/10
-            userPersonalityAgreeableness = data['personalityAgreeableness']/10
-            userPersonalityOpenness = data['personalityOpenness']/10
-            userPersonalityConscientiousness = data['personalityConscientiousness']/10
-            userPersonalityNeuroticism = data['personalityNeuroticism']/10
-            userPolitPref = data['politPref']
-            userIntPref = data['intPref']
-            # userAreaPref = data['areaPref']
-            userAreaPrefReformed = []
-            # for i in userAreaPref:
-            #     userAreaPrefReformed.append(i['value'])
-            userPersPref = data['persPref']
-            userGoals = data['goals']['value']
-            if (userGoals == 'FR'):
-                userGoals = ChatGoal.FRIENDSHIP
-            elif (userGoals == 'RO'):
-                userGoals = ChatGoal.ROMANTIC
-            else:
-                userGoals = ChatGoal.ANYTHING
-            userGenderPref = data['genderPref']['value']
-            if (userGenderPref == 'M'):
-                userGenderPref = Gender.MALE
-            elif (userGenderPref == 'F'):
-                userGenderPref = Gender.FEMALE
-            else:
-                userGenderPref = Gender.ANYTHING
-            userAgeRange = data['ageRange']
-            userAgeOptimal = data['ageOptimal']
-            
-            age_pref = AgePref.objects.create(min_age=userAgeRange[0], max_age=userAgeRange[1], optimal_age=userAgeOptimal)
-            age_pref.save()
-            personality = Personality.objects.create(
-                extraversion=userPersonalityExtraversion,
-                agreeableness=userPersonalityAgreeableness,
-                openness=userPersonalityOpenness,
-                conscientiousness=userPersonalityConscientiousness,
-                neuroticism=userPersonalityNeuroticism
-            )
-            personality.save()
-            pol = PolitCoordinates.objects.create(eco=userPolEco, cult=userPolGov)
-            pol.save()
-            geo = None
-            if (userLocToggle):
-                print('save geo')
-                geo = GeoCoordinates.objects.create(lat=userGeoLat, lon=userGeoLon)
-                geo.save()
-            user_info = UserInfo.objects.create(
-                interests=userInterestsReformed,
-                polit_coordinates=pol,
-                location=geo,
-                age=userAge if isinstance(userAge, int) else None,
-                gender=userGender,
-                personality=personality
-                )
-            user_info.save()
-            user_pref = Preferences.objects.create(
-                goals=userGoals,
-                age=age_pref,
-                polit=userPolitPref,
-                interests=userIntPref,
-                location=userLocPref,
-                gender=userGenderPref,
-                personality=userPersPref,
-                area_restrict=userAreaRestrictToggle,
-                loc_area=userAreaPref,
-                )
-            user_pref.save()
-            custom_user = CustomUser.objects.create(name=userName, user_info=user_info, user_prefs=user_pref)
-            custom_user.save()
+            custom_user = create_user_with_profile(data)
             print("Created CustomUser " + str(custom_user.pk))
             return JsonResponse({'user_id':custom_user.pk})
 
     def put(self, request):
         body_unicode = request.body.decode('utf-8')
         data = json.loads(body_unicode)
-        # Getting CustomUser data
-        image = data['image']
-        print('image')
-        print(image)
-        userId = data['user_id']
-        userName = data['name']
-        userDescription = data['description']
-        userAge = data['age']
-        userAreaRestrictToggle=data['areaRestrictToggle']
-        userLocPref=data['locPref']
-        userAreaPref=data['areaPref']
-        print('area pref')
-        print(userAreaPref)
-        userGender = data['gender']['value']
-        if (userGender == 'M'):
-            userGender = Gender.MALE
-        elif (userGender == 'F'):
-            userGender = Gender.FEMALE
-        else:
-            userGender = Gender.ANYTHING
-        userInterestsReformed = []
-        if 'interests' in data.keys():
-            userInterests = data['interests']
-            for i in userInterests:
-                userInterestsReformed.append(i['value'])
-        userGeoLat = data['geoLat']
-        print(userGeoLat)
-        userGeoLon = data['geoLon']
-        userPolEco = data['polEco']/10
-        userPolGov = data['polGov']/10
-        userPersonalityExtraversion = data['personalityExtraversion']/10
-        print(userPersonalityExtraversion)
-        userPersonalityAgreeableness = data['personalityAgreeableness']/10
-        userPersonalityOpenness = data['personalityOpenness']/10
-        userPersonalityConscientiousness = data['personalityConscientiousness']/10
-        userPersonalityNeuroticism = data['personalityNeuroticism']/10
-        userPolitPref = data['politPref']
-        userIntPref = data['intPref']
-        userLocPref = data['locPref']
-        userAreaPref = data['areaPref']
-        # userAreaPref = data['areaPref']
-        userAreaPrefReformed = []
-        # for i in userAreaPref:
-        #     userAreaPrefReformed.append(i['value'])
-        userPersPref = data['persPref']
-        userGoals = data['goals']['value']
-        if (userGoals == 'FR'):
-            userGoals = ChatGoal.FRIENDSHIP
-        elif (userGoals == 'RO'):
-            userGoals = ChatGoal.ROMANTIC
-        else:
-            userGoals = ChatGoal.ANYTHING
-        userGenderPref = data['genderPref']['value']
-        if (userGenderPref == 'M'):
-            userGenderPref = Gender.MALE
-        elif (userGenderPref == 'F'):
-            userGenderPref = Gender.FEMALE
-        else:
-            userGenderPref = Gender.ANYTHING
-        userAgeRange = data['ageRange']
-        userAgeOptimal = data['ageOptimal']
-
-        user = User.objects.get(id=userId)
-        user.username = userName
-        profile = user.profile
-
-        info = profile.user_info
-        info.interests=userInterestsReformed
-        info.description=userDescription
-        pol = info.polit_coordinates
-        pol.eco=userPolEco
-        pol.cult=userPolGov
-        pol.save()
-        info.polit_coordinates=pol
-        geo = info.location
-        geo.lat=userGeoLat
-        geo.lon=userGeoLon
-        geo.save()
-        info.location=geo
-        info.age=userAge
-        print(info.location.lat)
-        info.gender=userGender
-        personality=info.personality
-        personality.extraversion=userPersonalityExtraversion
-        personality.agreeableness=userPersonalityAgreeableness
-        personality.openness=userPersonalityOpenness
-        personality.conscientiousness=userPersonalityConscientiousness
-        personality.neuroticism=userPersonalityNeuroticism
-        print(personality.extraversion)
-        personality.save()
-        info.personality=personality
-        info.save()
-
-
-        prefs = profile.user_prefs
-        prefs.goals=userGoals
-        prefs.polit=userPolitPref
-        prefs.interests=userIntPref
-        prefs.location=userLocPref
-        prefs.gender=userGenderPref
-        prefs.personality=userPersPref
-        prefs.area_restrict=userAreaRestrictToggle
-        prefs.loc_area=userAreaPref
-        age_pref = prefs.age
-        age_pref.min_age=userAgeRange[0]
-        age_pref.max_age=userAgeRange[1]
-        age_pref.optimal_age=userAgeOptimal
-        age_pref.save()
-        prefs.age_pref = age_pref
-        prefs.save()
-
-        profile.user_info = info
-        profile.user_prefs = prefs
-        profile.save()
-        user.save()
-        print(user.profile.user_info.location.lat)
+        update_user(data)
         return JsonResponse({'good': 'good'})
 
 @method_decorator(csrf_exempt, name='dispatch')
